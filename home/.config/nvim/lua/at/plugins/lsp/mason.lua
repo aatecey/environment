e080533a501da10caf6d46dddadf379b10e92657
Aaron Tecey <atecey@gmail.com>
2025-01-26 04:17:14 +0000

    sync

--- home/.config/nvim/lua/at/plugins/lsp/mason.lua
+++ home/.config/nvim/lua/at/plugins/lsp/mason.lua
@@ -1,24 +1,34 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-	},
-	config = function()
-		local mason = require("mason")
-		local mason_lspconfig = require("mason-lspconfig")
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+  },
+  config = function()
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
 
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
 
-		mason_lspconfig.setup({
-			automatic_installaion = true,
-		})
-	end,
+    mason_lspconfig.setup({
+      automatic_installaion = true,
+      ensure_installed = {
+        "lua_ls",
+        "ts_ls",
+        "eslint",
+        "html",
+        "cssls",
+        "tailwindcss",
+        "jsonls",
+        "biome"
+      }
+    })
+  end,
 }
